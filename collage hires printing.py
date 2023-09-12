447ca1598027d2f5a59d792deb766467a4c6d1c3
Jmlunlocked <141883086+Jmlunlocked@users.noreply.github.com>
2023-09-12 17:34:33 +0100

    Add files via upload

--- collage hires printing.py
+++ collage hires printing.py
@@ -1,20 +1,21 @@
-path = "coll/"
+
 from PIL import Image
 import time
 import os
-from os import listdir
 import copy
 
 if not os.path.exists("coll"):
     # Create the folder
     os.mkdir("coll")
-    print("S")
 
 def main(repeats):
     faff = input("Welcome to the Collage Zone. Make sure you have added the pics to the folder 'coll'. The first time you answer this, it's probably best to just hit enter \nDo you want to faff? ")
     pix, min_side, params, area, equivalent = getpix()
     if equivalent:
-        return(equivalent_suite(pix, min_side, params, area, repeats))
+        print("Mkay - you're using equal sized pics, so let's see about that...")
+        equivalent = equivalent_suite(pix, min_side, params, area, repeats)
+        if equivalent:
+            return
     if (
         faff in {"Y", "y"}
         or len(faff) < 5
@@ -29,17 +30,18 @@ def main(repeats):
         and faff[0].lower() == "n"
     ):
         pix = getpix()[0]
-        printmostcompact(len(pix)**2, [0.5, 1, 1, 0])
-        return
+        return printmostcompact(len(pix)**2, [0.5, 1, 1, 0])
 
     return semi_advanced_suite(repeats)
 
 def printmostcompact(x, params):
+    not_equivalent = True
     min_bad = [[], 10**50]
+    bigrandomnumber = 997
     for i in range(x):
         pix, min_side, paramsthrowaway, area = getpix()
-        params[-1] = i + 997
-        print_complete = layout(copy.deepcopy(pix), min_side, params, area)
+        params[-1] = i + bigrandomnumber
+        print_complete = layout(copy.deepcopy(pix), min_side, params, area, not_equivalent)
         total_area = print_complete[1][0] * print_complete[1][1]
         if print_complete[2] * total_area < min_bad[-1]:
             print_complete[2] *= total_area
@@ -57,14 +59,15 @@ def coll(print_complete):
     collage.show()
     collage.save("coll"+str(int(time.time()))+".jpg",quality=99) 
 
-
+def isfloat(x):
+    try:
+        float(x)
+        return True
+    except ValueError:
+        return False
+    
 def advanced_suite(repeats):
-    def isfloat(x):
-        try:
-            float(x)
-            return True
-        except ValueError:
-            return False
+    not_equivalent = True
 
     pix, min_side, params, area = getpix()
     displayed = (
@@ -75,14 +78,15 @@ def advanced_suite(repeats):
     )
     print("this probably won't produce the best results (try trying something different next time)")
     params = [0, 0, 0, 0]
+    bigrandomnumber = 997
     for rep in range(repeats):
         for i in range(len(displayed)):
             text = input(displayed[i])
             if i == 0:
                 if rep != 0:
                     if text.lower() == "rs":
-                        params[-1] += 1000
-                        print_complete = layout(copy.deepcopy(pix), min_side, params, area)
+                        params[-1] += bigrandomnumber
+                        print_complete = layout(copy.deepcopy(pix), min_side, params, area, not_equivalent)
                         coll(print_complete)
                         break
                 continue
@@ -93,7 +97,7 @@ def advanced_suite(repeats):
         if text.lower() == "rs":
             continue
         params[-1] = 0
-        print_complete = layout(copy.deepcopy(pix), min_side, params, area)
+        print_complete = layout(copy.deepcopy(pix), min_side, params, area, not_equivalent)
         coll(print_complete)
         
 def semi_advanced_suite(repeats):
@@ -120,14 +124,14 @@ def semi_advanced_suite(repeats):
         printmostcompact(len(pix)**2, params)
         
 def getpix():
-    global path
+    path = "coll/"
     pix = []
     area = 0
     dimensions = (0, 0)
     equivalent = 0
     
     for i in os.listdir(path):
-        if i == ".DS_Store":
+        if i in {".DS_Store"}:
             continue
         img = Image.open(path + i)
         x, y = img.size
@@ -144,11 +148,11 @@ def getpix():
         equivalent = False
     for i in range(len(pix)):
         pix[i].append(i)
-    min_length = int(area**0.5)
-    return pix, min_length, [1, 1, 1, 0], area, equivalent
+    min_side = int(area**0.5) + 1
+    return pix, min_side, [1, 1, 1, 0], area, equivalent
 
 
-def layout(pix, min_side, params, area):
+def layout(pix, min_side, params, area, not_equivalent):
     widest = [0, 0, 0, 0, 0]
     tallest = [0, 0, 0, 0, 0]
     sprawlingest = [0]
@@ -157,9 +161,9 @@ def layout(pix, min_side, params, area):
             widest = pic
         if pic[2] > tallest[2]:
             tallest = pic
-        area = pic[1] * pic[2]
-        if area > sprawlingest[0]:
-            sprawlingest = [area, pic]
+        pic_area = pic[1] * pic[2]
+        if pic_area > sprawlingest[0]:
+            sprawlingest = [pic_area, pic]
     wide = []
     tall = []
     min_max_side = min_side
@@ -173,23 +177,26 @@ def layout(pix, min_side, params, area):
             tall.append(pic)
     if min_max_side > min_side:
         min_side = min_max_side
-    min_side = int(min_side * 1.3)
+    if not_equivalent:
+        min_side = int(min_side * 1.3)
     
     if len(wide) + len(tall) == 0 and len(pix) < 4:
         pass
-    orientation = 0
+    orientation = 1
     if tallest[2] > widest[1]:
-        orientation = 1
+        orientation = 0
     if tallest[2] * 1.5 > widest[1] and len(tall) > 0:
-        orientation = 1
+        orientation = 0
+    if not not_equivalent:
+        orientation = 0
     widest_tallest = [widest, tallest]
     print_complete = draw(
-        pix, orientation, sprawlingest[1], widest_tallest, params, min_side, area
+        pix, orientation, sprawlingest[1], widest_tallest, params, min_side, area, not_equivalent
     )
     return print_complete
 
 
-def draw(pix, orientation, sprawlingest, widest_tallest, params, min_side, area):
+def draw(pix, orientation, sprawlingest, widest_tallest, params, min_side, area, not_equivalent):
     # determine which pic to add next
     def nextpic(pix, print_folder, min_side, border, orientation, aspect):
         
@@ -207,7 +214,7 @@ def draw(pix, orientation, sprawlingest, widest_tallest, params, min_side, area)
                 + print_folder[-1][1 + orientation]
                 + print_folder[-1][3 + orientation]
                 + border
-                < min_side
+                <= min_side
             ):
                 candidates.append(pic)
         if len(candidates) == 0:
@@ -221,7 +228,7 @@ def draw(pix, orientation, sprawlingest, widest_tallest, params, min_side, area)
             + print_folder[-2][orientation + 1]
             + print_folder[-2][orientation + 3]
             + border
-            < min_side
+            <= min_side
         ):
             print_folder[-1][orientation + 3] = (
                 print_folder[-2][orientation + 1]
@@ -246,7 +253,8 @@ def draw(pix, orientation, sprawlingest, widest_tallest, params, min_side, area)
 
     print_folder = []
     border = int(((area / len(pix))**0.5) * params[0])
-    min_side += border * len(print_folder)**0.5
+    if not_equivalent:
+        min_side += border * len(print_folder)**0.5
     top = int(((area / len(pix))**0.5) * params[1])
     side = int(((area / len(pix))**0.5) * params[2])
     aspect = 1
@@ -310,25 +318,21 @@ def centreofmass(print_folder, x, y):
     return unbalancedness + total_area**0.5
 
 def equivalent_suite(pix, min_side, params, area, repeats):
+    not_equivalent = False
     n = len(pix)
     candidates = []
     
     for i in range(1, n + 1):
-        if n % i == 0 and pix[0][1] * n / i <= 2 * min_side and pix[0][1] * n / i >= 0.5 * min_side:
+        if n % i == 0 and pix[0][1] * n / i < 2 * min_side and pix[0][1] * n / i > 0.5 * min_side:
             candidates.append((i, pix[0][1] * n / (i**2 * pix[0][2])))
-    if candidates == []:
-        return semi_advanced_suite(repeats)
+    if len(candidates) == 0:
+        return False
+    
     candidate = (0, 11)
     for grouping in candidates:
         if grouping[1] + 1 / grouping[1] < candidate[1]:
             candidate = (grouping[0], grouping[1] + 1 / grouping[1])
-
-    def isfloat(x):
-        try:
-            float(x)
-            return True
-        except ValueError:
-            return False
+    
 
     displayed = (
         "do you want to reshuffle? Type 'rs' ",
@@ -346,21 +350,23 @@ def equivalent_suite(pix, min_side, params, area, repeats):
                         params[-1] += 997
                         border = int(((area / len(pix))**0.5) * params[0])
                         min_side = candidate[0] * pix[0][1] + (candidate[0] - 1) * border
-                        print_complete = layout(copy.deepcopy(pix), min_side, params, area)
+                        print_complete = layout(copy.deepcopy(pix), min_side, params, area, not_equivalent)
                         coll(print_complete)
                         break
                 continue
             while not isfloat(text) or float(text) < -5 or float(text) > 19.9:
                 print("enter a number between 0.1 and 10")
                 text = input(displayed[i])
-            params[i - 1] = float(text)
+            params[i - 1] = float(text) / 4
         if text.lower() == "rs":
             continue
         params[-1] = 0
         border = int(((area / len(pix))**0.5) * params[0])
         min_side = candidate[0] * pix[0][1] + (candidate[0] - 1) * border
-        print_complete = layout(copy.deepcopy(pix), min_side, params, area)
-        coll(print_complete)  
+        print(min_side, border)
+        print_complete = layout(copy.deepcopy(pix), min_side, params, area, not_equivalent)
+        coll(print_complete)
+    return True
     
 if __name__ == "__main__":
     main(10)
